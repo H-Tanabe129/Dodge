61b3ce5123361d9ad150593f2219a2151e5c3aee
ひなの 田邊 <220130@jc-21.jp>
2024-02-26 11:35:08 +0900

    シーン追加

---
+++ TitleScene.cpp
@@ -0,0 +1,28 @@
+#include "TitleScene.h"
+#include "Engine/Image.h"
+
+TitleScene::TitleScene(GameObject* parent)
+  : GameObject(parent, "TitleScene"), hPict_(-1)
+{
+}
+
+void TitleScene::Initialize()
+{
+    //画像データのロード
+    hPict_ = Image::Load("ファイル名");
+    assert(hPict_ >= 0);
+}
+
+void TitleScene::Update()
+{
+}
+
+void TitleScene::Draw()
+{
+    Image::SetTransform(hPict_, transform_);
+    Image::Draw(hPict_);
+}
+
+void TitleScene::Release()
+{
+}
