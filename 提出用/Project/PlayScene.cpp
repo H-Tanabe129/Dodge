5d9c2ddf8776b0f8038c6b448b325b845c03615f
ひなの 田邊 <220130@jc-21.jp>
2024-03-07 11:52:51 +0900

    提出用ファイル作成

---
+++ 提出用/Project/PlayScene.cpp
@@ -0,0 +1,40 @@
+#include "Engine/Camera.h"
+#include "PlayScene.h"
+#include "Player.h"
+#include "Stage.h"
+#include "Obstacle.h"
+#include "BackGround.h"
+
+//コンストラクタ
+PlayScene::PlayScene(GameObject * parent)
+	: GameObject(parent, "PlayScene")
+{
+}
+
+//初期化
+void PlayScene::Initialize()
+{
+	Camera::SetPosition(XMFLOAT3(15, 3, -25));  //test == 35, 3, 0    -25
+	Camera::SetTarget(XMFLOAT3(15, 0, 10));
+	
+	Instantiate<Player>(this);
+	Instantiate<Stage>(this);
+	//Instantiate<Obstacle>(this);
+
+	//Instantiate<BackGround>(this);
+}
+
+//更新
+void PlayScene::Update()
+{
+}
+
+//描画
+void PlayScene::Draw()
+{
+}
+
+//開放
+void PlayScene::Release()
+{
+}
---
+++ 提出用/Project/StageUp.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Engine/GameObject.h"
+
+class StageUp : public GameObject
+{
+private:
+    int hModel_;    //モデル番号
+    //int width;
+    //int height;
+    //int depth;
+public:
+    //コンストラクタ
+    StageUp(GameObject* parent);
+
+    //デストラクタ
+    ~StageUp();
+
+    //初期化
+    void Initialize() override;
+
+    //更新
+    void Update() override;
+
+    //描画
+    void Draw() override;
+
+    //開放
+    void Release() override;
+
+    //DirectX::XMFLOAT3 GetScale() override { return GameObject::GetScale(); }
+};
