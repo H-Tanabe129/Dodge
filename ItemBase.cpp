987533a766f8ec51002679225aa68b3f49da085f
ひなの 田邊 <220130@jc-21.jp>
2024-06-27 15:11:07 +0900

    基底と派生を作りたい

---
+++ ItemBase.cpp
@@ -0,0 +1,59 @@
+#include "ItemBase.h"
+
+ItemBase::ItemBase(GameObject* parent)
+    :GameObject(parent, "ItemBase"), hModel_(-1), hSound_(-1)
+{
+}
+
+ItemBase::~ItemBase()
+{
+}
+
+void ItemBase::Initialize()
+{
+    transform_.position_.x = trPosiX;
+
+    //サウンドデータのロード
+    hSound_ = Audio::Load("A1_18278.WAV");
+    assert(hSound_ >= 0);
+
+    BoxCollider* collision = new BoxCollider
+    (XMFLOAT3(BCollPosiX, BCollPosiY, BCollPosiZ),
+        XMFLOAT3(BCollSizeX, BCollSizeY, BCollSizeZ));
+    AddCollider(collision);
+}
+
+void ItemBase::Update()
+{
+    transform_.position_.x -= trPosiChangeX;
+    transform_.rotate_.y += trRoteChangeY;
+
+    if (transform_.position_.x <= LEdge) {
+        this->KillMe();
+    }
+}
+
+void ItemBase::Draw()
+{
+    Model::SetTransform(hModel_, transform_);
+    Model::Draw(hModel_);
+}
+
+void ItemBase::Release()
+{
+}
+
+void ItemBase::OnCollision(GameObject* pTarget)
+{
+    if (pTarget->GetObjectName() == "Player") {
+        this->KillMe();
+        Audio::Play(hSound_);
+    }
+
+    if (pTarget->GetObjectName() == "StageUp") {
+        this->KillMe();
+    }
+    if (pTarget->GetObjectName() == "StageLo") {
+        this->KillMe();
+    }
+}
---
+++ ItemBase.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Engine/GameObject.h"
+#include "Engine/BoxCollider.h"
+#include "Engine/Model.h"
+#include "Engine/Audio.h"
+
+class ItemBase : public GameObject
+{
+protected:
+    int hModel_;
+    int hSound_;
+
+    int trPosiX = 35;      //モデルポジションX
+    int trPosiY = 0;       //モデルポジションY
+    int trPosiZ = 0;       //モデルポジションZ
+
+    const float BCollPosiX = 0.5;    //コライダーポジションX
+    const float BCollPosiY = 0.5;    //コライダーポジションY
+    const float BCollPosiZ = 0;      //コライダーポジションZ
+    const float BCollSizeX = 1.1;    //コライダーサイズX
+    const float BCollSizeY = 1.1;    //コライダーサイズY
+    const float BCollSizeZ = 1.1;    //コライダーサイズZ
+
+    float trPosiChangeX = 0.1;      //X座標変化量
+    float trRoteChangeY = 0.5;      //Y軸回転変化量
+    static const int LEdge = -10;   //世界左端
+
+public:
+    //コンストラクタ
+    ItemBase(GameObject* parent);
+
+    //デストラクタ
+    virtual ~ItemBase();
+
+    //初期化
+    virtual void Initialize() override;
+
+    //更新
+    virtual void Update() override;
+
+    //描画
+    virtual void Draw() override;
+
+    //開放
+    virtual void Release() override;
+
+    // 衝突処理
+    virtual void OnCollision(GameObject* pTarget) override;
+};
